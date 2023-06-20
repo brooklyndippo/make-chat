f2126fb841c940cc90596391524722361bf470dc
Brooklyn Dippo <brooklyndippo@gmail.com>
2023-06-19 21:35:34 -1000

    integrated socket/io

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-//App.js
-const express = require('express');
-const app = express();
-//Socket.io has to use the http server
-const server = require('http').Server(app);
-
+//app.js
 //Express View Engine for Handlebars
+const exphbs  = require('express-handlebars');
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+//Establish your public folder
+app.use('/public', express.static('public'))
+
+//Socket.io
+const io = require('socket.io')(server);
+io.on("connection", (socket) => {
+  console.log("🔌 New user connected! 🔌");
+})
+
 const exphbs  = require('express-handlebars');
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
